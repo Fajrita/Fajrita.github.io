11f555978677c4f9c50edbef89bf3a843d82f302
Fernanda Fajre <ffajrec@gmail.com>
2026-06-19 19:43:31 -0400

    new page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
